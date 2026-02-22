25997bab413689dadd756607c65c94f5ded0af2d
himanshu.kumar.993345 <himanshu.kumar.993345@gmail.com>
2026-02-22 16:05:12 +0000

    added index.ejs

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-// create a basic express server
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
 
 let v = 0;
 
 app.get("/", (req, res) => {
     v++;
     console.log(v);
-    res.send("ok. working");
+    res.render("index", {v});
 });
 
 app.listen(port, "0.0.0.0", ()=> {
