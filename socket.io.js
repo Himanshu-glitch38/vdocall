7f1c5e7db490f03db222c55115115e49fc2c6cfc
Himanshu-glitch38 <himanshu.kumar.993345@gmail.com>
2026-03-02 05:36:19 +0530

    Update socket.io.js

--- socket.io.js
+++ socket.io.js
@@ -1,11 +1,26 @@
 module.exports = (io) => {
-
+  let waitingQueue = [];
   let users = 0;
   // console.log(io);
     io.on('connection', (socket) => {
       users++;
+      if (waitingQueue.length > 0) {
+      const partner = waitingQueue.shift();
+
+      //socket.partner = partner.id;
+      //partner.partner = socket.id;
+
+      //socket.emit("matched", partner.id);
+      //partner.emit("matched", socket.id);
+    } else {
+      waitingQueue.push(socket);
+      socket.emit("waiting");
+      }
+      
       socket.on('disconnect', () => {
         users--;
+        waitingQueue = waitingQueue.filter(s => s.id !== socket.id);
+        
       });
 
       socket.on("getUserCount", (feedback) => {
