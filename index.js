8953098342aec6ade4b33e1175fb3e87a8a4b87b
Himanshu-glitch38 <himanshu.kumar.993345@gmail.com>
2026-03-02 06:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ global.db = db;
 const express = require('express');
 const http = require("http");
 const { Server } = require("socket.io");
-
+const path = require("path");
+    
 const app = express();
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set('view engine', 'ejs');
 
